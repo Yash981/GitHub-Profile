ec2a57093f960664e331e63396504041d5a57cf4
Yashwanth <dyashwanth14011999@gmail.com>
2024-02-09 20:47:55 +0530

    updated search outline

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -40,7 +40,7 @@ const Search = () => {
   
   return (
     <>
-    <form className={`SearchInput flex justify-center items-center bg-[#364153] rounded-lg px-2 py-2 w-fit mx-auto mt-10 focus-within:outline-[2px] focus-within:outline-[#3662E3]`}>
+    <form className={`SearchInput flex justify-center items-center bg-[#364153] rounded-xl px-2 py-2 w-fit mx-auto mt-10 focus-within:outline focus-within:outline-[2px] focus-within:outline-[#3662E3]`}>
         <img src={SearchImg} alt="searchImage" className='px-2 py-1' />
         <input type="text"  id='search' name='search' className='outline-none bg-transparent text-[#CDD5E0] md:w-[31rem] py-2 max-md:w-[20rem]' value={searchUser} placeholder='username'
         onChange={e=>handleInput(e.target.value)}
